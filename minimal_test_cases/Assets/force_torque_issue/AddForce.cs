b7a8b1bde56e4cdcaf3b8be0ded7cd7229104ccf
Christopher Sahadeo <chris@virtanatech.com>
2020-02-21 14:18:28 -0400

    Adding unrealisitic physics in Unity test scene

---
+++ minimal_test_cases/Assets/force_torque_issue/AddForce.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    public float forceStrength;
+    public float RayDistance;
+
+    // empty child object of Magnet - used to set the raycast begin point in world
+    private Transform _tipTransform;
+    
+    void Start()
+    {
+        // set force strength
+        if (forceStrength <= 0)
+        {
+            forceStrength = 5;
+            Debug.Log("Set defualt force: " + forceStrength);
+        }
+
+        // set ray cast distance (max distance at which the code will check for a collision with rigidbody)
+        if(RayDistance <= 0)
+        {
+            RayDistance = 1f;
+            Debug.Log("Set defualt ray distance: " + RayDistance);
+        }
+
+        // get tip transform
+        _tipTransform = gameObject.transform.Find("Tip");
+        if (_tipTransform == null)
+        {
+            Debug.Log("The 'Tip' frame was not found!");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // setup raycast
+        Vector3 rayBeginInTip = Vector3.zero;
+        Vector3 rayBeginInWorld = _tipTransform.TransformPoint(rayBeginInTip);
+        Vector3 rayDirectionInWorld = _tipTransform.TransformDirection(Vector3.up); // ray is casted out in the positive y direction in tip frame
+
+        // cast ray out from tip frame
+        RaycastHit raycastHitInfo;
+        bool hit = Physics.Raycast(rayBeginInWorld, rayDirectionInWorld, out raycastHitInfo, RayDistance);
+        Debug.DrawRay(rayBeginInWorld, rayDirectionInWorld * RayDistance, Color.green, 1f, false);
+
+        // get hit info
+        Vector3 hitPointInWorld = raycastHitInfo.point;
+        Rigidbody hitObject = raycastHitInfo.rigidbody;
+
+        // apply force at hit point in direction of tip
+        Vector3 force = -rayDirectionInWorld.normalized * forceStrength;
+        hitObject.AddForceAtPosition(force, hitPointInWorld);
+        Debug.DrawRay(hitPointInWorld, force, Color.blue, 1f, false);       
+    }
+}
+
---
+++ minimal_test_cases/Assets/force_torque_issue/readme.md
@@ -0,0 +1,34 @@
+# Unrealistic physics in Unity when applying forces
+*Tested in Unity 2020.1.0a22*
+
+## Scene description and usage
+
+This scene demonstrates unrealistic physics when applying forces to rigidbodies.
+
+The scene comprises of 2 main `GameObject`s:
+- a small item (cuboid)
+- a magnet (this is not intended to be an acurate model of a magnet but is sufficient to demonstrate this issue)
+
+Run the scene and translate the magnet along the z axis (sliding the magnet across the item).
+
+The magnet casts a ray out from its tip. A force is applied using [`Rigidbody.AddForceAtPosition`](https://docs.unity3d.com/ScriptReference/Rigidbody.AddForceAtPosition.html) at the point in the world where the raycast intersects with the collider of the rigidbody in front of it (the small item).
+
+On manually translating the magnet along the z axis (sliding the magnet across the item), we see the following behaviour:
+1. A weak force (2 units) acting on a 0.1616kg mass produces reasonable behaviour, with the item pivoting downward under the influence of gravity as the magnet is translated to either edge:
+![Good](./good.png)
+2. On applying a larger force (5 units), the item will unintuitively begin to pivot upward as the gripper approaches the edge:
+![Bad](./bad.png)
+
+We suspect this is a result of inaccruate force/torque calculations within Unity.
+
+No Physic Materials were applied, thus eliminating friction between item and magnet, which facilitates the observation of the issue.
+
+**NB:** making the following changes to `DynamicsManager.asset` results in similar behaviour:
+```
+m_DefaultMaxDepenetrationVelocity: 10
+m_DefaultSolverIterations: 25
+m_DefaultSolverVelocityIterations: 25
+m_SolverType: 1
+```
+
+
